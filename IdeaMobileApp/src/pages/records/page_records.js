476c1b3b666119a04c0b5efc8d56bd9f7b55a615
edgar <edgar.quintero@ua.pt>
2023-07-17 14:55:45 +0100

    histórico 3 atividades, label mobilidade

--- IdeaMobileApp/src/pages/records/page_records.js
+++ IdeaMobileApp/src/pages/records/page_records.js
@@ -1,67 +1,168 @@
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Pressable, ScrollView, Text, View, TextInput } from "react-native";
 import { useEffect, useState } from "react";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
+import { AirLabel, EnergyLabel, MovementLabel, RecycleLabel, WaterLabel } from "../../components/labels.js";
 
-import { styles } from "../../assets/styles/css.js"
-import * as CONST from "../../assets/constants/constants.js"
-
-import { AirBox, EnergyBox, MovementBox, RecycleBox, WaterBox } from "../../components/areaBoxes.js";
+import { styles } from "../../assets/styles/css.js";
+import * as CONST from "../../assets/constants/constants.js";
 import { AddButton } from "../../components/buttons.js";
 
-export default function Records({ route, navigation }) {
+export default function Records({ navigation}) {
+  //const { category, activeCategories } = route.params;
+  //console.log(activeCategories.length)
+  const [colors, setColors]= useState ([CONST.purple, CONST.yellow, CONST.pink, CONST.green, CONST.blue])
 
-    //const { category, activeCategories } = route.params;
-    //console.log(activeCategories.length)
-    const [area, setArea] = useState("")
-    const [points, setPoints] = useState(60)
-    const [colorArea, setColorArea] = useState()
-    const [days, setDays] = useState([])
+  //const pickDate = new Date();
 
-
-    return(
-       <SafeAreaProvider style={[styles.mainContainer]}>
-            <StatusBar style={"dark"} />
-            <View>
-                <Text style={styles.indicatorTitle}>
-                    Histórico de Atividades
-                </Text>
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-            <Text style={styles.descriptionText}>
-                Aqui podes visualizar o histórico dos teus comportamentos sustentáveis!
+  return (
+    <SafeAreaProvider style={[styles.mainContainer]}>
+      <StatusBar style={"dark"} />
+      <View>
+        <Text style={styles.indicatorTitle}>Histórico de Atividades</Text>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.descriptionText}>
+          Aqui podes visualizar o histórico dos teus comportamentos
+          sustentáveis!
+        </Text>
+        <View style={styles.cardBox}>
+          <View
+            style={{
+              flexDirection: "column",
+              marginBottom: CONST.inputFieldMargin,
+            }}
+          >
+            <Text
+              style={[
+                styles.normalText,
+                { marginBottom: 0, textAlign: "left" },
+              ]}
+            >
+              Para editar ou remover uma atividade basta pressionar
+              continuamente alguns segundos.
             </Text>
-                <View style={styles.cardBox}>
-                    <View style={{ flexDirection: 'column', marginBottom: CONST.inputFieldMargin }}>
-                        <Text style={[styles.normalText, { marginBottom: 0, textAlign: 'left' }]}>Para editar ou remover uma atividade basta pressionar continuamente alguns segundos.</Text>
-                    </View>
-                    <View style={{ 
-                        flexDirection: 'row',
-                        borderTopWidth: 1,
-                        borderColor: "#DDD",
-                        justifyContent: 'space-between',
-                        alignItems: 'center', alignContent: 'center', alignSelf: 'center',
-                        width: '100%'
-                        }}>
-                        <View >
-                            <Text style={[styles.normalText, { marginTop: CONST.normalTextMargin, marginBottom: 0 }]}>Escolhe o dia a ser apresentado. </Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
-                            <Text style={[styles.normalText, { marginTop: CONST.normalTextMargin, marginBottom: 0 }]}>14/07</Text>
-                         <FontAwesome5 name="calendar-alt" size={CONST.heading5} color={CONST.mainGray}/>
-                        </View>
-                    </View>
-                </View>
-                <View style={[styles.cardBox, { marginTop: CONST.boxCardMargin }]}>
-                    <View style={{ marginBottom: CONST.boxCardMargin }}>
-                        <Text style={[styles.subText, { marginBottom: 0, textAlign: 'left' }]}>Label Climatização</Text>
-                    </View>
-                </View>
-            </ScrollView>
-        </SafeAreaProvider>
-    
-    )
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              borderTopWidth: 1,
+              borderColor: "#DDD",
+              justifyContent: "space-between",
+              alignItems: "center",
+              alignContent: "center",
+              alignSelf: "center",
+              width: "100%",
+            }}
+          >
+            <View>
+              <Text
+                style={[
+                  styles.normalText,
+                  { marginTop: CONST.normalTextMargin, marginBottom: 0 },
+                ]}
+              >
+                Escolhe o dia a ser apresentado.{" "}
+              </Text>
+            </View>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text
+                style={[
+                  styles.normalText,
+                  { marginTop: CONST.normalTextMargin, marginBottom: 0 },
+                ]}
+              >
+                14/07
+              </Text>
+              <FontAwesome5
+                name="calendar-alt"
+                size={CONST.heading5}
+                color={CONST.mainGray}
+              />
+            </View>
+          </View>
+        </View>
+        <View style={[styles.cardBox, { marginTop: CONST.boxCardMargin }]}>
+          <View>
+            <View style={{ flexDirection: 'row'}}>
+              <AirLabel color={colors[0]} />
+            </View>
+            <View>
+            <Text
+                style={[
+                  styles.normalText,
+                  { marginTop: CONST.normalTextMargin, marginBottom: 0 },
+                ]}
+              >
+              O  ar condicionado esteve ligado?
+              </Text>
+              <Text
+                style={[
+                  styles.normalText,
+                  { fontFamily: 'K2D-SemiBold', marginBottom: 0 },
+                ]}
+              >
+              Nunca.
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={[styles.cardBox, { marginTop: CONST.boxCardMargin }]}>
+          <View>
+            <View style={{ flexDirection: 'row'}}>
+              <EnergyLabel color={colors[1]} />
+            </View>
+            <View>
+            <Text
+                style={[
+                  styles.normalText,
+                  { marginTop: CONST.normalTextMargin, marginBottom: 0 },
+                ]}
+              >
+              As luzes estiveram desligadas?
+              </Text>
+              <Text
+                style={[
+                  styles.normalText,
+                  { fontFamily: 'K2D-SemiBold', marginBottom: 0},
+                ]}
+              >
+              Sempre.
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={[styles.cardBox, { marginTop: CONST.boxCardMargin }]}>
+          <View>
+            <View style={{ flexDirection: 'row'}}>
+              <MovementLabel color={colors[2]} />
+            </View>
+            <View>
+            <Text
+                style={[
+                  styles.normalText,
+                  { marginTop: CONST.normalTextMargin, marginBottom: 0 },
+                ]}
+              >
+              A tua deslocação para o trabalho foi como?
+              </Text>
+              <Text
+                style={[
+                  styles.normalText,
+                  { fontFamily: 'K2D-SemiBold', marginBottom: 0},
+                ]}
+              >
+              Viatura Partilhada.
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaProvider>
+  );
 }
